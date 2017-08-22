d2a6b557c5c8786cb960ee297d98fae02936a6b1
Enrico Tuvera Jr <enrico@elephantventures.com>
2017-08-22 19:42:24 +0800

    first commit!

---
+++ cookbook/ingredients/schema.py
@@ -0,0 +1,23 @@
+import graphene
+from graphene_django.types import DjangoObjectType
+from ingredients.models import Category, Ingredient
+
+
+class CategoryType(DjangoObjectType):
+    class Meta:
+        model = Category
+
+class IngredientType(DjangoObjectType):
+    class Meta:
+        model = Ingredient
+
+class Query(graphene.AbstractType):
+    all_categories = graphene.List(CategoryType)
+    all_ingredients = graphene.List(IngredientType)
+
+    def resolve_all_categories(self, args, context, info):
+        return Category.objects.all()
+
+    def resolve_all_ingredients(self, args, context, info):
+        return Ingredient.objects.select_related('category').all()
+
