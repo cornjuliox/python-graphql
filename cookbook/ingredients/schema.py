a661bf2c1c2d8bbac74d120b5582d95b34f56e67
Enrico Tuvera Jr <enrico@elephantventures.com>
2017-08-22 20:38:18 +0800

    added gitignore and updated schema

--- cookbook/ingredients/schema.py
+++ cookbook/ingredients/schema.py
@@ -15,9 +15,36 @@ class Query(graphene.AbstractType):
     all_categories = graphene.List(CategoryType)
     all_ingredients = graphene.List(IngredientType)
 
+    category = graphene.Field(CategoryType, id=graphene.Int(), name=graphene.String())
+    ingredient = graphene.Field(IngredientType, id=graphene.Int(), name=graphene.String())
+
     def resolve_all_categories(self, args, context, info):
         return Category.objects.all()
 
     def resolve_all_ingredients(self, args, context, info):
         return Ingredient.objects.select_related('category').all()
+
+    def resolve_category(self, args, context, info):
+        id = args.get('id')
+        name = args.get('name')
+
+        if id is not None:
+            return Category.objects.get(pk=id)
+
+        if name is not None:
+            return Category.objects.get(name=name)
+
+        return None
+
+    def resolve_ingredient(self, args, context, info):
+        id = args.get('id')
+        name = args.get('name')
+
+        if id is not None:
+            return Ingredient.objects.get(pk=id)
+
+        if name is not None:
+            return Ingredient.objects.get(name=name)
+
+        return None
 
